ddf36e5864fdcc0b1f908a393e31df17d2bf8d89
안현모 <blue_pope@hotmail.com>
2019-11-06 17:23:59 +0900

    키후팅 키입력 테스트

---
+++ ConsoleKeyHooker/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleKeyHooker
+{
+    class Program
+    {
+        /*
+         WH_JOURNALRECORD = 0,
+            WH_JOURNALPLAYBACK = 1,
+            WH_KEYBOARD = 2,
+            WH_GETMESSAGE = 3,
+            WH_CALLWNDPROC = 4,
+            WH_CBT = 5,
+            WH_SYSMSGFILTER = 6,
+            WH_MOUSE = 7,
+            WH_HARDWARE = 8,
+            WH_DEBUG = 9,
+            WH_SHELL = 10,
+            WH_FOREGROUNDIDLE = 11,
+            WH_CALLWNDPROCRET = 12,
+            WH_KEYBOARD_LL = 13,
+            WH_MOUSE_LL = 14
+             */
+
+        private const int WH_GETMESSAGE = 3;
+        private const int WH_KEYBOARD_LL = 13;
+
+        private const int WM_PAINT = 15;
+        private const int WM_KEYDOWN = 0x0100;
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        static void Main(string[] args)
+        {
+            _hookID = SetHook(_proc);
+
+            Application.Run();
+
+            UnhookWindowsHookEx(_hookID);
+
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+
+            {
+
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                Console.WriteLine((Keys)vkCode);
+
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
---
+++ ConsoleKeySender/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleKeySender
+{
+    class Program
+    {
+
+        [DllImport("user32.dll")]
+        public static extern uint FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern uint FindWindowEx(uint hWnd1, uint hWnd2, string lpsz1, string lpsz2);
+
+        [DllImport("user32.dll")]
+        public static extern uint SendMessage(uint hwnd, uint wMsg, uint wParam, uint lParam);
+
+        [DllImport("user32.dll")]
+        public static extern uint PostMessage(uint hwnd, uint wMsg, uint wParam, uint lParam);
+
+        const uint WM_KEYDOWN = 0x100;
+        const uint WM_KEYUP = 0x101;
+        const uint WM_CHAR = 0x102;
+        const uint WM_PASTE = 0x0302;
+        const uint WM_SYSCOMMAND = 0x018;
+        const uint SC_CLOSE = 0x053;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            GCHandle a = 
+            uint handle;
+
+            //핸들을 찾는다. Spy+를 통해 찾은 클래스 이름과 캡션을 이용하면 된다. 둘 중 하나만 알경우에도 찾을 수 있다. 그때는 하나의 인자를 null로 넘겨 주면된다.
+            handle = FindWindow(null, "닷넷 C# 개발자 모임");
+            //찾은 핸들에서 자식 윈도우 핸들을 찾기 위해서는 FindWindowEx를 이용한다.
+
+            //EVA_VH_ListControl_Dblclk //화면창
+
+            //RichEdit20W 텍스트창
+            handle = FindWindowEx(handle, 0, "RichEdit20W", null);
+
+            Clipboard.SetText("테스트..");
+            PostMessage(handle, WM_PASTE, 0, 0);
+            PostMessage(handle, WM_KEYDOWN, 0xD, 0); //enter
+        }
+    }
+}
