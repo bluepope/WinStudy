d7840d53e74a9da1ba8987b770c48f05f9314438
안현모 <blue_pope@hotmail.com>
2019-11-14 18:07:22 +0900

    fody 없이 테스트

---
+++ WpfApp1/DelegateCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        public bool IsExecuting { get; private set; } = false;
+        Action<T> executeTargets = delegate { };
+        Func<bool> canExecuteTargets = delegate { return false; };
+
+        public bool CanExecute(object parameter)
+        {
+            return !IsExecuting && (canExecuteTargets.GetInvocationList().Length == 1 || (canExecuteTargets?.Invoke() ?? true));
+        }
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter) == false)
+                return;
+
+            IsExecuting = true;
+            CommandManager.InvalidateRequerySuggested();
+
+            if (parameter?.GetType().Name == "SelectedItemCollection")
+            {
+                var list = (IList)parameter;
+                var arr = new T[list.Count];
+
+                ((IList)parameter).CopyTo(arr, 0);
+
+                foreach (var item in arr)
+                {
+                    executeTargets((T)item);
+                }
+            }
+            else
+            {
+                executeTargets(parameter != null ? (T)parameter : default(T));
+            }
+            
+
+            IsExecuting = false;
+            CommandManager.InvalidateRequerySuggested();
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public event Action<T> ExecuteTargets
+        {
+            add => executeTargets += value;
+            remove => executeTargets -= value;
+        }
+        public event Func<bool> CanExecuteTargets
+        {
+            add => canExecuteTargets += value;
+            remove => canExecuteTargets -= value;
+        }
+    }
+}
---
+++ WpfApp1/VMTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    class VMTest1 : INotifyPropertyChanged
+    {
+        string __text1;
+        public string Text1 { get => __text1; set { __text1 = value; OnPropertyChanged(); } }
+
+        int __num1;
+        public int Num1 { get => __num1; set { __num1 = value; OnPropertyChanged(); } }
+
+        int __num2;
+        public int Num2 { get => __num2; set { __num2 = value; OnPropertyChanged(); } }
+
+        public bool Check1 { get; set; }
+        public bool Check2 { get; set; }
+
+        public string List1SelectedValue { get; set; }
+
+        public DateTime DateTime1 { get; set; } = DateTime.Now;
+        public string DateText1 { get; set; } = DateTime.Now.ToString("yyyyMMdd");
+
+        public List<KeyValuePair<string, string>> List1 { get; set; } = new List<KeyValuePair<string, string>>();
+
+        public DelegateCommand<object> Button1Command { get; set; } = new DelegateCommand<object>();
+
+        public VMTest1()
+        {
+            Button1Command.ExecuteTargets += Button1Command_ExecuteTargets;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void Button1Command_ExecuteTargets(object obj)
+        {
+            Text1 = new Random().Next().ToString();
+            Num1 = new Random().Next(100);
+            Num2 = new Random(Num1).Next(100);
+        }
+
+    }
+}
