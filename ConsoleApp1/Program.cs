40ad28b69bf72607a4f53ba36dda9acc0ae358d4
안현모 <blue_pope@hotmail.com>
2020-02-28 09:26:16 +0900

    MemoryMappedFile 테스트 해봄

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -7,6 +9,34 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            using (var file = System.IO.MemoryMappedFiles.MemoryMappedFile.CreateFromFile("D:/backup/새 폴더/20181211.GHO"))
+            {
+                using (var accessor = file.CreateViewAccessor())
+                {
+                    var bufferSize = 4096000;
+                    var buffer = new byte[bufferSize];
+                    long position = 0;
+
+                    var stopwatch = new Stopwatch();
+
+                    stopwatch.Start();
+
+                    while(accessor.Capacity > position)
+                    {
+                        accessor.ReadArray(position, buffer, 0, bufferSize);
+                        
+                        position += bufferSize;
+                        
+                        //Console.WriteLine(BitConverter.ToString(buffer));
+                    }
+
+                    stopwatch.Stop();
+                    Console.WriteLine(stopwatch.Elapsed.ToString(@"hh\:mm\:ss"));
+
+                }
+            }
+
+            /*
             using (var db = new SqlDataHelper("Data Source=.;Initial Catalog=MyDB;Integrated Security=SSPI;"))
             {
                 db.Execute("INSERT INTO Customer(id, name, phone) VALUES(@id, @name, @phone)", new { id = 1, name = "이순신", phone = "010-433-3111" });
@@ -30,6 +60,7 @@ namespace ConsoleApp1
                     Console.WriteLine();
                 }
             }
+            */
         }
     }
 }
