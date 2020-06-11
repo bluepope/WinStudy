8a33a2534564912d49a45cc6be73692400e7e728
안현모 <blue_pope@hotmail.com>
2020-06-11 11:27:55 +0900

    윈폼 코어 디자이너 나와서 테스트 해봄 데이터 그리드 아직 지원 안됨

---
+++ WinFormCore/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormCore
+{
+    public partial class Form1 : Form
+    {
+        CancellationTokenSource _cts = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            progressBar1.Maximum = 100;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_cts != null)
+            {
+                _cts.Cancel();
+            }
+
+            var cts = new CancellationTokenSource();
+
+            Task.Run(async () => {
+                for(int i=0; i <= 100; i += 10)
+                {
+                    if (cts.IsCancellationRequested)
+                        return;
+
+                    progressBar1.Invoke(new MethodInvoker(() =>
+                    {
+                        progressBar1.Value = i;
+                    }));
+
+                    await Task.Delay(500);
+                }
+            }, cts.Token);
+
+            _cts = cts;
+            
+            //textBox1.Text = "test";
+        }
+    }
+}
--- WpfCore/MainWindow.xaml.cs
+++ WpfCore/MainWindow.xaml.cs
@@ -20,9 +20,28 @@ namespace WpfCore
     /// </summary>
     public partial class MainWindow : Window
     {
+        ViewModel1 _viewModel = new ViewModel1();
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = _viewModel;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Text1 = new Random().Next(1000000).ToString();
+            _viewModel.Num1 = new Random().Next(100);
+        }
+    }
+
+    public class ViewModel1 : BaseModel
+    {
+        string _text1;
+
+        public string Text1 { get => _text1; set { _text1 = value; OnPropertyChanged();  } }
+
+        int _num1;
+        public int Num1 { get => _num1; set { _num1 = value; OnPropertyChanged(); } }
     }
 }
