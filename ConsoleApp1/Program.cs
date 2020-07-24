2c4fa7c06e4c0313f32cda77138883a507a539fb
안현모 <blue_pope@hotmail.com>
2020-07-24 16:48:12 +0900

    정규식 테스트

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -33,7 +34,20 @@ namespace ConsoleApp1
             //new HMacTest().Run();
             //new SystemInfoTest().Run();
 
-            new PropertyTest().Run();
+            //new PropertyTest().Run();
+
+            //단순 정규식으로 문자숫자가 들어왔을때 첫번째 숫자 기준으로 나누기
+            string str = "TRaADF%!123,123,123".Trim();
+            int index = Regex.Match(str, "[0-9]").Index;
+
+            string text = "";
+            decimal amt = 0;
+
+            if (index > 0)
+                text = str.Substring(0, index);
+
+            amt = Convert.ToDecimal(str.Substring(index).Replace(",", ""));
+            Console.WriteLine($"{text} - {amt}");
 
             Console.ReadKey();
 
